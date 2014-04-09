d352893b680ca635e7f5d4aa50fcb3b3ebe7e1ef
Gallaxya <Gallaxya@192-168-0-100.rdsnet.ro>
2014-04-10 01:04:13 +0300

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Gallaxya on 10/04/14.
+//  Copyright (c) 2014 Gallaxya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Gallaxya on 10/04/14.
+//  Copyright (c) 2014 Gallaxya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
